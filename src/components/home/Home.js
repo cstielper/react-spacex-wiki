fad898a8f6083a2fde80e907a01116f5d413d746
cstielper <chris@mm4solutions.com>
2018-08-31 11:47:22 -0400

    Changes to home page layout

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,8 +3,71 @@ import Helmet from 'react-helmet';
 import styled from 'styled-components';
 
 const Article = styled.article`
+  position: relative;
+  min-height: 100%;
+  padding: 0 0 2rem !important;
+
+  &,
+  & a {
+    color: #fff;
+    text-shadow: 1px 1px 3px rgba(0, 0, 0, 1);
+  }
+
+  &:after {
+    content: '';
+    display: block;
+    position: absolute;
+    z-index: 2;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    background: rgba(0, 0, 0, 0.5);
+  }
+
+  .article-header,
+  .article-wrapper {
+    position: relative;
+    z-index: 5;
+    padding: 0 2rem;
+
+    h1 {
+      padding-top: 1.5rem;
+      margin-top: 0;
+    }
+  }
+
+  > img {
+    position: absolute;
+    z-index: 1;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
   section > h2 {
     margin-bottom: 0.25rem;
+  }
+
+  @media (min-width: 46em) {
+    .article-header,
+    .article-wrapper {
+      padding-right: 25rem;
+    }
+
+    .article-header {
+      h1 {
+        padding-top: 3.5rem;
+      }
+    }
+  }
+
+  @media (min-width: 62em) {
+    .article-header {
+      h1 {
+        padding-top: 6.5rem;
+      }
+    }
   }
 `;
 
@@ -25,7 +88,7 @@ class Home extends React.Component {
 
   render() {
     return (
-      <div className="component-wrapper">
+      <div className="component-wrapper component-home">
         <Helmet>
           <title>SpaceX Info</title>
           <meta
@@ -34,16 +97,13 @@ class Home extends React.Component {
           />
         </Helmet>
 
-        <figure className="feature-img">
+        <Article>
           <img
             src="images/satellite-sm.jpg"
             srcSet="images/satellite-sm.jpg 400w, images/satellite-md.jpg 800w, images/satellite-lg.jpg 1200w, images/satellite-xl.jpg 2200w"
             sizes="100vw"
             alt="SpaceX satellite"
           />
-        </figure>
-
-        <Article>
           <header className="article-header">
             <h1>About SpaceX</h1>
           </header>
